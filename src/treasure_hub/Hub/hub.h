7665faec294bcd079361110d731876913abfb730
Daria <daria.ciobanu@student.upt.ro>
2025-05-06 19:03:17 +0300

    structures done

---
+++ src/treasure_hub/Hub/hub.h
@@ -0,0 +1,67 @@
+#ifndef __HUB_H__
+#define __HUB_H__
+
+// ---------------------------------------------------------
+// Includes
+// ---------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/wait.h>
+
+// ---------------------------------------------------------
+// Defines
+// ---------------------------------------------------------
+
+#define TREASURE_MANAGER_EXEC  "bin/treasure_manager"
+#define TREASURE_MONITOR_EXEC  "bin/treasure_monitor"
+
+// ---------------------------------------------------------
+// Structures
+// ---------------------------------------------------------
+
+typedef enum{
+    INVALID_OPERATION,
+    CLEAR,
+    HELP,
+    EXIT,
+    START_MONITOR,
+    STOP_MONITOR,
+    LIST_HUNTS,
+    LIST_TREASURES,
+    VIEW_TREASURE,
+}Hub_Command_T;
+
+typedef enum{
+    OFF, 
+    RUNNING, 
+    SHUTTING_DOWN
+} Status_t;
+
+typedef struct{
+    pid_t monitor_pid;
+    Status_t monitor_status;
+    int monitor_fd;
+}Hub_Monitor_T;
+
+// ---------------------------------------------------------
+// external functions
+// ---------------------------------------------------------
+
+
+// ---------------------------------------------------------
+// extern variable
+// ---------------------------------------------------------
+
+extern Hub_Monitor_T hub_monitor;
+
+
+#endif
